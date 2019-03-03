f281cf59dedd678725b1a6dd07e58af5670a214c
Herlilan <47225920+Herlilan@users.noreply.github.com>
2019-03-02 23:02:13 -0800

    Update horizontal-lines.js

--- horizontal-lines.js
+++ horizontal-lines.js
@@ -4,7 +4,11 @@ function setup() {
 
 function draw() {
   background(220);
-  for (var i = 10; i <= 390; i = i + 10) {
-    line(10, i, 390, i);
+  for (var i = 20; i <= 380; i = i + 10) {
+    var startX = 10;
+    var startY = i;
+    var endX = 390;
+    var endY = i;
+    line(startX, startY, endX, endY);
   }
 }
