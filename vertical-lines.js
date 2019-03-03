1c606559bb98a85d681e42d909421abcd307c65d
Herlilan <47225920+Herlilan@users.noreply.github.com>
2019-03-02 23:01:38 -0800

    Update vertical-lines.js

--- vertical-lines.js
+++ vertical-lines.js
@@ -4,7 +4,11 @@ function setup() {
 
 function draw() {
   background(220);
-  for (var i = 10; i <= 390; i = i + 10) {
-    line(i, 10, i, 390);
+  for (var i = 20; i <= 380; i = i + 10) {
+    var startX = i;
+    var startY = 10;
+    var endX = i;
+    var endY = 390;
+    line(startX, startY, endX, endY);
   }
 }
